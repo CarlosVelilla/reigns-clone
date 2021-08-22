b9e3d7e8049f0a016165f4520367c340c99a4f98
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-08-22 11:18:31 +0200

    Add form to make inputs directly into the DB & Add basic BD structure with placeholders

---
+++ assets/add-card-form/functions.js
@@ -0,0 +1,79 @@
+const URL = 'http://localhost:3000'
+const FORM = document.getElementById('addCardForm')
+
+FORM.addEventListener('submit', (event) => {
+  event.preventDefault()
+  let formData = new FormData(FORM)
+  let parentCardText = (formData.get('card-parent') == "") ? false : formData.get('card-parent')
+  let advice = (formData.get('card-advice') == "") ? false : formData.get('card-advice')
+
+  fetch(`${URL}/cards`, {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+      id: formData.get('card-id'),
+      parentCard: parentCardText
+    })})
+
+  fetch(`${URL}/content`, {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+      id: formData.get('card-id'),
+      title: formData.get('card-title'),
+      advice: advice
+    })})
+
+  fetch(`${URL}/character`, {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+      id: formData.get('card-id'),
+      name: formData.get('character-name'),
+      background: formData.get('character-background'),
+      url: formData.get('character-url')
+    })})
+
+  fetch(`${URL}/accept`, {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+      id: formData.get('card-id'),
+      toCard: formData.get('card-toCardAccept'),
+      text: formData.get('card-textAccept')
+    })})
+
+  fetch(`${URL}/decline`, {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+      id: formData.get('card-id'),
+      toCard: formData.get('card-toCardDecline'),
+      text: formData.get('card-textDecline')
+    })})
+    
+  fetch(`${URL}/modifiers`, {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+      id: `${formData.get('card-id')}-accept`,
+      religion: parseInt(formData.get('religion-accept')),
+      population: parseInt(formData.get('population-accept')),
+      army: parseInt(formData.get('army-accept')),
+      money: parseInt(formData.get('money-accept')),
+      magic: parseInt(formData.get('magic-accept')),
+    })})
+        
+  fetch(`${URL}/modifiers`, {
+    method: 'POST',
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({
+      id: `${formData.get('card-id')}-decline`,
+      religion: parseInt(formData.get('religion-decline')),
+      population: parseInt(formData.get('population-decline')),
+      army: parseInt(formData.get('army-decline')),
+      money: parseInt(formData.get('money-decline')),
+      magic: parseInt(formData.get('magic-decline')),
+    })})
+
+})
