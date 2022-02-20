15fa07b5be39651cd4850fe74f92ce078d9d1a78
CarlosVelilla <carlosvelillaf@hotmail.com>
2022-02-20 23:07:58 +0100

    Change URL for endpoints

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,25 +1,26 @@
-import { gameModeModifier, gameMode } from "./main.js"
-import { gameOverWindow } from "./gameover.js"
+import { gameModeModifier, gameMode } from './main.js';
+import { gameOverWindow } from './gameover.js';
 
 /* GLOBAL VARIABLES */
-const URL = "http://localhost:3000";
+// const URL = "http://localhost:3000";
+const URL = 'https://your-highness.herokuapp.com';
 
 function createCard(cardId, firstCard) {
-  const ACCEPTBTN = document.getElementById("accept-button");
-  const DECLINEBTN = document.getElementById("decline-button");
+  const ACCEPTBTN = document.getElementById('accept-button');
+  const DECLINEBTN = document.getElementById('decline-button');
 
   /* LOCATING ELEMENTS IN THE HIDDEN CARD SIDE */
-  let hiddencard = document.querySelector("[data-displayed='false']")
-  const TEXT = document.querySelector("[data-text]");
-  const ADVICE = document.querySelector("[data-advice]");
-  let CHARACTERIMAGE
-  let CHARACTERNAME
+  let hiddencard = document.querySelector("[data-displayed='false']");
+  const TEXT = document.querySelector('[data-text]');
+  const ADVICE = document.querySelector('[data-advice]');
+  let CHARACTERIMAGE;
+  let CHARACTERNAME;
   if (firstCard === false) {
-    CHARACTERIMAGE = document.querySelector("[data-image]");
-    CHARACTERNAME = document.querySelector("[data-name]");
+    CHARACTERIMAGE = document.querySelector('[data-image]');
+    CHARACTERNAME = document.querySelector('[data-name]');
   } else {
-    CHARACTERIMAGE = hiddencard.querySelector("[data-image]");
-    CHARACTERNAME = hiddencard.querySelector("[data-name]");
+    CHARACTERIMAGE = hiddencard.querySelector('[data-image]');
+    CHARACTERNAME = hiddencard.querySelector('[data-name]');
   }
 
   /* ASSIGN NEXT CARD ID TO BUTTONS */
@@ -27,17 +28,17 @@ function createCard(cardId, firstCard) {
   fetch(`${URL}/accept/${cardId}`)
     .then((response) => response.json())
     .then((data) => {
-      ACCEPTBTN.setAttribute("data-tocard", data.toCard)
-      ACCEPTBTN.setAttribute("data-cardid", cardId)
-      ACCEPTBTN.textContent = data.text
+      ACCEPTBTN.setAttribute('data-tocard', data.toCard);
+      ACCEPTBTN.setAttribute('data-cardid', cardId);
+      ACCEPTBTN.textContent = data.text;
     });
 
   fetch(`${URL}/decline/${cardId}`)
     .then((response) => response.json())
     .then((data) => {
-      DECLINEBTN.setAttribute("data-tocard", data.toCard)
-      DECLINEBTN.setAttribute("data-cardid", cardId)
-      DECLINEBTN.textContent = data.text
+      DECLINEBTN.setAttribute('data-tocard', data.toCard);
+      DECLINEBTN.setAttribute('data-cardid', cardId);
+      DECLINEBTN.textContent = data.text;
     });
 
   /* PRINTING CARD TITLE AND ADVICE */
@@ -46,7 +47,7 @@ function createCard(cardId, firstCard) {
     .then((data) => {
       TEXT.innerHTML = data.text;
       data.advice === false
-        ? (ADVICE.textContent = "")
+        ? (ADVICE.textContent = '')
         : (ADVICE.textContent = data.advice);
     });
 
@@ -61,92 +62,99 @@ function createCard(cardId, firstCard) {
 }
 
 function nextCard(event) {
-  editScore(event)
+  editScore(event);
   /* We use a small timeout to give time to the editScore function to run */
   setTimeout(() => {
-    let currentTotalPoints = getCurrentPointsTotal()
-    editProgressBar(currentTotalPoints)
-    let gameOver = isGameOver(currentTotalPoints)
-    if (gameOver == "continue") {
+    let currentTotalPoints = getCurrentPointsTotal();
+    editProgressBar(currentTotalPoints);
+    let gameOver = isGameOver(currentTotalPoints);
+    if (gameOver == 'continue') {
       createCard(event.target.dataset.tocard);
-      toggleDataDisplayed()
-      document.getElementById("flip-card--inner").classList.toggle("rotate--plus");
-    } else if (gameOver == "fail" || gameOver == "success") {
+      toggleDataDisplayed();
+      document
+        .getElementById('flip-card--inner')
+        .classList.toggle('rotate--plus');
+    } else if (gameOver == 'fail' || gameOver == 'success') {
       gameOverWindow(gameOver, currentTotalPoints);
     }
-  }, 50)
+  }, 50);
 }
 
 function toggleDataDisplayed() {
-  let hiddencard = document.querySelector("[data-displayed='false']")
-  let showedcard = document.querySelector("[data-displayed='true']")
-  hiddencard.dataset.displayed="true"
-  showedcard.dataset.displayed="false"
+  let hiddencard = document.querySelector("[data-displayed='false']");
+  let showedcard = document.querySelector("[data-displayed='true']");
+  hiddencard.dataset.displayed = 'true';
+  showedcard.dataset.displayed = 'false';
 }
 
 function editScore(event) {
-  let cardId = event.target.dataset.cardid
-  let action = event.target.dataset.action
+  let cardId = event.target.dataset.cardid;
+  let action = event.target.dataset.action;
   fetch(`${URL}/modifiers/${cardId}-${action}`)
-  .then((response) => response.json())
-  .then((data) => Object.keys(data).forEach(factor => {
-    if (factor != "id") {
-      editFactor(factor, parseInt(data[factor]))
-    }
-  }));
+    .then((response) => response.json())
+    .then((data) =>
+      Object.keys(data).forEach((factor) => {
+        if (factor != 'id') {
+          editFactor(factor, parseInt(data[factor]));
+        }
+      })
+    );
 }
 
 function editFactor(factor, points) {
-  let element = document.querySelector(`[data-icon="${factor}"]`)
-  let currentPoints = getCurrentPointsByFactor(factor)
-  let totalPoints = points+currentPoints
+  let element = document.querySelector(`[data-icon="${factor}"]`);
+  let currentPoints = getCurrentPointsByFactor(factor);
+  let totalPoints = points + currentPoints;
   if (totalPoints > 10) {
-    totalPoints = 10
+    totalPoints = 10;
   } else if (totalPoints < 0) {
-    totalPoints = 0
+    totalPoints = 0;
   }
-  element.dataset.points = totalPoints
-  element.classList.remove(`pa-${currentPoints}`)
-  element.classList.add(`pa-${totalPoints}`)
+  element.dataset.points = totalPoints;
+  element.classList.remove(`pa-${currentPoints}`);
+  element.classList.add(`pa-${totalPoints}`);
 }
 
 function getCurrentPointsByFactor(factor) {
-  let element = document.querySelector(`[data-icon="${factor}"]`)
-  let result = element.dataset.points
-  return parseInt(result)
+  let element = document.querySelector(`[data-icon="${factor}"]`);
+  let result = element.dataset.points;
+  return parseInt(result);
 }
 
 function getCurrentPointsTotal() {
-  let factors = document.querySelectorAll(`[data-points]`)
-  let totalPoints = 0
-  factors.forEach(factor => {
-    totalPoints += parseInt(factor.dataset.points)
-  })
-  return totalPoints
+  let factors = document.querySelectorAll(`[data-points]`);
+  let totalPoints = 0;
+  factors.forEach((factor) => {
+    totalPoints += parseInt(factor.dataset.points);
+  });
+  return totalPoints;
 }
 
 function editProgressBar(points) {
   // console.log("Points "+points);
-  document.documentElement.style.setProperty("--progress-bar", (points-15)*gameModeModifier);
+  document.documentElement.style.setProperty(
+    '--progress-bar',
+    (points - 15) * gameModeModifier
+  );
   // console.log("Variable "+getComputedStyle(document.documentElement).getPropertyValue("--progress-bar"));
 }
 
 function pointsToWin(mode) {
   switch (mode) {
     case 'mode-easy':
-      return 5
+      return 5;
     case 'mode-medium':
-      return 10
+      return 10;
     case 'mode-hard':
-      return 15
+      return 15;
   }
 }
 
 function isGameOver(totalPoints) {
-  let pointsToSuccess = 15 + pointsToWin(gameMode)
-  if (totalPoints >= pointsToSuccess) return "success"
-  else if (totalPoints > 0) return "continue"
-  else if (totalPoints <= 0) return "fail"
+  let pointsToSuccess = 15 + pointsToWin(gameMode);
+  if (totalPoints >= pointsToSuccess) return 'success';
+  else if (totalPoints > 0) return 'continue';
+  else if (totalPoints <= 0) return 'fail';
 }
 
 export { createCard, nextCard };
