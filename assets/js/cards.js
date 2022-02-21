3968d06a05b82302bc745018ea5c0911da749816
CarlosVelilla <carlosvelillaf@hotmail.com>
2022-02-21 23:09:04 +0100

    Finishing and cleaning

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -28,7 +28,6 @@ function createCard(cardId, firstCard) {
   fetch(`${URL}/accept/${cardId}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       ACCEPTBTN.setAttribute('data-tocard', data.toCard);
       ACCEPTBTN.setAttribute('data-cardid', data.id);
       ACCEPTBTN.textContent = data.text;
@@ -37,7 +36,6 @@ function createCard(cardId, firstCard) {
   fetch(`${URL}/decline/${cardId}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       DECLINEBTN.setAttribute('data-tocard', data.toCard);
       DECLINEBTN.setAttribute('data-cardid', data.id);
       DECLINEBTN.textContent = data.text;
@@ -47,7 +45,6 @@ function createCard(cardId, firstCard) {
   fetch(`${URL}/content/${cardId}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       TEXT.innerHTML = data.text;
       data.advice === false
         ? (ADVICE.textContent = '')
@@ -58,7 +55,6 @@ function createCard(cardId, firstCard) {
   fetch(`${URL}/character/${cardId}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       CHARACTERIMAGE.src = data.url;
       CHARACTERNAME.textContent = data.name;
       CHARACTERIMAGE.style.backgroundColor = data.background;
@@ -97,7 +93,6 @@ function editScore(event) {
   fetch(`${URL}/modifiers/${cardId}-${action}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       Object.keys(data).forEach((factor) => {
         if (factor != 'id') {
           editFactor(factor, parseInt(data[factor]));
